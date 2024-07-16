7572e683e42abf670ec788df3b795d899b225924
toeten <164077137+toeten@users.noreply.github.com>
2024-07-16 13:28:25 -0400

    k elements

---
+++ 5. Top-K-Frequent-Elements/Nicole.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+        for (const num of nums) {
+            if(map.has(num)) map.set(num, map.get(num) + 1)
+            else map.set(num, 1)
+            //console.log(map)
+        }
+        const freqArray = Array.from(map.entries())
+        //console.log(freqArray)
+        freqArray.sort((a,b) => b[1] - a[1])
+        //console.log(freqArray)
+        const result = freqArray.slice(0, k).map(pair => pair[0])
+        return result
+    }
+}
