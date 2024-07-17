9b37606b30ed35481398fbbb582cb893986a0419
toeten <164077137+toeten@users.noreply.github.com>
2024-07-17 11:28:40 -0400

    comments

--- 1-Contains-Duplicates/Nicole.js
+++ 1-Contains-Duplicates/Nicole.js
@@ -4,12 +4,12 @@ class Solution {
 	 * @return {boolean}
 	 */
 	hasDuplicate(nums) {
-		const set = new Set();
+		const set = new Set(); //create a new set
 
-		for (const num of nums) {
-			if (set.has(num)) return true;
-			set.add(num);
+		for (const num of nums) { //loop through the array
+			if (set.has(num)) return true; //if the set contains the num return true 
+			set.add(num); //add num to the set 
 		}
-		return false;
+		return false; //if not, return false
 	}
 }
