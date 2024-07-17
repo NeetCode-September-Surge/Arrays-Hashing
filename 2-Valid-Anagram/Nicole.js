bea60fe2e084643d3527b125579335812f870711
toeten <164077137+toeten@users.noreply.github.com>
2024-07-17 11:35:00 -0400

    Update Nicole.js

--- 2-Valid-Anagram/Nicole.js
+++ 2-Valid-Anagram/Nicole.js
@@ -5,7 +5,9 @@ class Solution {
 	 * @return {boolean}
 	 */
 	isAnagram(s, t) {
-		if (s.length !== t.length) return false;
-		return s.split("").sort().join("") === t.split("").sort().join("");
+		if (s.length !== t.length) return false; //if not the same length they cant be anagrams
+		return s.split("").sort().join("") === t.split("").sort().join(""); //split s at each letter, sort and join them back into a string
+		//do the same for t
+		//if they match, theyre anagrams
 	}
 }
